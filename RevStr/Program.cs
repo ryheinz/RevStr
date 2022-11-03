f7b331bc329c275881b64b700fdad9aa1a1a3538
ryheinz <ryan.heinz@gmail.com>
2022-11-03 15:39:25 +0100

    test change

--- RevStr/Program.cs
+++ RevStr/Program.cs
@@ -6,7 +6,7 @@ namespace RevStr // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Helloo World!");
         }
     }
 }
