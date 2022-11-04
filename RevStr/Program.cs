d8cef1a47a8abb54b997e647e41e4abb83eb1e3b
ryheinz <ryan.heinz@gmail.com>
2022-11-04 23:43:33 +0100

    Update Program.cs
    first attempt

--- RevStr/Program.cs
+++ RevStr/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace RevStr // Note: actual namespace depends on the project name.
+namespace RevStr /* Note: 1.enter word in console
+                          2. take that word and create string
+                          3. create array wth the letters of that string
+                          4. write line of the array in reverse order */
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloo World!");
+            Console.WriteLine("Please enter any word");
+            String = Entered.Word(Console.ReadLine());
+            int[] somearray =
+
+
+
+
         }
     }
 }
